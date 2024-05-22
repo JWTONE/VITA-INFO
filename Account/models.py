13776ccd3814b75345642396fd4d5f3cc344ef04
BeomJung Kim <kbeom0124@naver.com>
2024-05-22 19:03:36 +0900

    Add : 프로젝트 초기세팅
    Account(계정) 앱 추가하고 모델 생성
    Survey(설문조사) 앱 추가하고 모델 생성
    Post(게시글) 앱 추가하고 모델 생성
    Settings.py 수정
    1.앱 추가 및 장고 3rd Party 추가
    2.RESTFULframework 설정
    3.AUTH_USER_MODEL 설정
    4.SIMPLE_JWT 로그인 토큰 설정
    
    .gitignore 생성

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    name = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    email = models.EmailField()
+    gender = models.Choices(choices=GENDER_CHOICES, max_length=1)
+    date_of_birth = models.DateField()
+    subscription = models.BooleanField()
+
--- Post/models.py
+++ Post/models.py
@@ -1,3 +1,27 @@
+from typing import Self
 from django.db import models
+from Account.models import User
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(upload_to='')
+    category = models.CharField(max_length=30)
+    author = models.ForeignKey(User,on_delete=models.CASCADE, related_name="post")
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(
+        User, to_field='nickname', on_delete=models.CASCADE, related_name='comment_author')
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment_likes = models.ManyToManyField(User,related_name='comment_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #대댓글
+    is_reply = models.BooleanField()
+    depth = models.IntegerField(null=True, blank=True)
+    comment_id = models.ForeignKey(Self,on_delete=models.CASCADE, related_name='replies', null=True, blank=True)
