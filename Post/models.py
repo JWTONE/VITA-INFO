2534c5f0400a872fe0d5f6e823e7bf9c1d14d15c
BeomJung Kim <kbeom0124@naver.com>
2024-05-22 19:50:29 +0900

    update : Post모델 수정
    Post 모델에 이미지 저장, 유저별 경로 설정
    Settings.py에 미디어 경로 추가
    requirements.txt 생성

--- Post/models.py
+++ Post/models.py
@@ -3,10 +3,16 @@ from django.db import models
 from Account.models import User
 from django.conf import settings
 
+def get_image_upload_path(instance, filename):
+    if instance.user.is_authenticated:
+        return f".reviews/{filename}"
+    else: 
+        return f".admins/{filename}"
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    image = models.ImageField(upload_to="")
+    image = models.ImageField(upload_to=get_image_upload_path)
     category = models.CharField(max_length=30)
     author = models.ForeignKey(User,on_delete=models.CASCADE, related_name="post")
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_posts")
