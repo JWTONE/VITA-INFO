6bbf1eccdf1e64bf1fd62183f0f734a3f9a36f1c
xogns3725 <xogns3725@naver.com>
2024-05-23 11:57:53 +0900

    Update : Logout 기능 추가
    logout url 설정 추가
    views에 로그아웃 클래스 추가

--- Account/views.py
+++ Account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .serializers import UserSerializer
 
@@ -14,3 +16,15 @@ class UserListAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        if request.data:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        else:    
+            return Response(status=status.HTTP_400_BAD_REQUEST)
