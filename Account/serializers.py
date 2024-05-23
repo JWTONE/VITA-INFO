f6b55ab6abe5d994e6cc3222d4dcf6ef4b762c1e
xogns3725 <xogns3725@naver.com>
2024-05-23 15:37:45 +0900

    Update : 비밀번호 변경 기능 추가
    urls 경로 추가
    views PasswordUpdateAPIView 추가, put요청 시 비밀번호 변경 가능
    serializers에 UserCreateSerialier 상속받아 비밀번호 변경 기능 추가

--- Account/serializers.py
+++ Account/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
 
 class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,18 +11,19 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         fields = [
             "email", "name", "nickname", "date_of_birth", "gender", "subscription"
         ]
-        
+
+
 class UserCreateSerializer(UserUpdateSerializer):
     password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True, required=True)
-    
+
     class Meta:
         model = User
         fields = [
-            "username", "password", "confirm_password", "email", "name", 
+            "username", "password", "confirm_password", "email", "name",
             "nickname", "date_of_birth", "gender", "subscription"
         ]
-            
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
@@ -29,7 +32,7 @@ class UserCreateSerializer(UserUpdateSerializer):
         password = data.get("password")
         username = data.get("username")
         confirm_password = data.pop("confirm_password", None)
-        
+
         if username == password:
             raise serializers.ValidationError("아이디와 비밀번호를 다르게 입력해주세요.")
         if not password:
@@ -38,4 +41,29 @@ class UserCreateSerializer(UserUpdateSerializer):
             raise serializers.ValidationError("비밀번호가 서로 일치하지 않습니다.")
         validate_password(password)
         return data
-    
+
+
+class UserPasswordSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = ["password", "confirm_password"]
+
+    def validate(self, data):
+        new_password = data.get("password")
+        password = data.get("password")
+        confirm_password = data.pop("confirm_password", None)
+        validate_password(new_password)
+        user = self.instance
+        if user and check_password(new_password, user.password):
+            raise ValidationError("비밀번호가 이전과 같습니다.")
+        if not password:
+            raise serializers.ValidationError("비밀번호를 입력해주세요.")
+        if password != confirm_password:
+            raise serializers.ValidationError("비밀번호가 서로 일치하지 않습니다.")
+        validate_password(password)
+        return data
+
+    def update(self, instance, validated_data):
+        instance.password = make_password(validated_data["password"])
+        instance.save()
+        return instance
