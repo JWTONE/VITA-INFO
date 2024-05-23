5e8695160cdea9a846fc8b27a27a64defcf20f30
xogns3725 <xogns3725@naver.com>
2024-05-23 15:17:31 +0900

    Update : 회원정보 수정 기능 추가
    urls 경로 추가
    views에 put 요청 시 회원정보 수정 가능
    userserializer를 update, create로 분리함

--- Account/urls.py
+++ Account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", UserListAPIView.as_view(), name="user_list"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("<str:username>/", UserListAPIView.as_view(), name="profile_detail"),
 ]
--- Account/views.py
+++ Account/views.py
@@ -6,21 +6,33 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserCreateSerializer, UserUpdateSerializer
 
 class UserListAPIView(APIView):
     def get_object(self, username):
         return get_object_or_404(User, username=username)
     
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = UserCreateSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
+    def put(self, request, username):
+        user = self.get_object(username)
+        if request.user.username == user.username:
+            serializer = UserUpdateSerializer(
+                user, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    
     def delete(self, request):
-        user = get_object_or_404(User, username=request.user.username)
-        password = request.data.get('password')
+        user = self.get_object(request.user.username)
+        password = request.data.get("password")
         if check_password(password, user.password):
             user.delete()
             return Response(status=status.HTTP_200_OK)
