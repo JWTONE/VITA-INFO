269f5c4bc6caa93257fa5edcae31c773f7e4cbb1
hsji <jihyosang@yonsei.ac.kr>
2024-05-24 11:08:36 +0900

    Update: 게시글 조회
    views PostListAPIView를 통해 게시물 조회 기능 추가
    urls 게시물 조회 url 추가
    serializers 게시물 조회 관련 serializers 추가

--- Post/models.py
+++ Post/models.py
@@ -1,4 +1,3 @@
-from typing import Self
 from django.db import models
 from Account.models import User
 from django.conf import settings
--- Post/urls.py
+++ Post/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
+
+app_name = 'post'
 
 urlpatterns = [
-    
+     path("", views.PostListAPIView.as_view(), name="post_list"),
 ]
