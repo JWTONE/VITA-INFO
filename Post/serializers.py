1e90657cc01f563313285323a270e8ed3ee96ab9
hsji <jihyosang@yonsei.ac.kr>
2024-05-24 11:38:27 +0900

    Update: 게시글 작성 기능 추가
    serializers 게시글 작성 기능을 위한 소스코드 변경
    views 게시글 작성 기능을 위한 소스코드 변경

--- Post/serializers.py
+++ Post/serializers.py
@@ -3,19 +3,16 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    like_count = serializers.SerializerMethodField()
+#    like_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = [
             "id",
             "title",
-            "username",
+            "author",
             "content",
             "created_at",
             "updated_at",
-            "like_count",
         ]
-
-    def get_like_count(self, obj):
-        return obj.like.count()
+        read_only_fields = ['author',]
