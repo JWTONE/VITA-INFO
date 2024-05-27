6ec7581daf7486bb9391d3daa47d4c5b25ba5aff
BeomJung Kim <kbeom0124@naver.com>
2024-05-27 15:56:27 +0900

    Update: AI활용 설문결과 출력 기능
    views에 설문 내용 토대로 결과 출력 해주는 로직 구현

--- Survey/views.py
+++ Survey/views.py
@@ -1,17 +1,28 @@
+import json
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from Survey.serializers import SurveySerializer
 from rest_framework.permissions import IsAuthenticated
-
+from .function import survey
 
 class SurveyAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
         user = request.user
-        print(user.username)
         serializer = SurveySerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
+            query = serializer.validated_data
+            result = survey(query)
+            try:
+                result_json = json.loads(result)  # result를 JSON 형식으로 변환
+            except (TypeError, json.JSONDecodeError):
+                result_json = {'error': 'Invalid result format'}
             serializer.save(user=user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            response_data = {
+                'survey': serializer.data,
+                'result': result_json
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        
