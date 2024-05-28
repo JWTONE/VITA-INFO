69c0a8ee6a23d5d6706b897c148468a8167f7b81
BeomJung Kim <kbeom0124@naver.com>
2024-05-28 11:22:47 +0900

    Update: 기타 입력칸 기능
    serializers에 설문 선택지 중 기타를 선택시 기타 내용 입력 기능 구현
    requirements.txt 최신화

--- Survey/serializers.py
+++ Survey/serializers.py
@@ -6,6 +6,15 @@ class SurveySerializer(serializers.ModelSerializer):
     class Meta:
         model = SurveyInfo
         exclude = ["user"]
+    
+    def validate(self, data):
+        if data.get('snacks') == 'OTHER' and not data.get('snacks_other'):
+            raise serializers.ValidationError({"기타": "기타란에 입력하세요"})
+        if data.get('health_goals') == 'OTHER' and not data.get('health_goals_other'):
+            raise serializers.ValidationError({"기타": "기타란에 입력하세요"})
+        if data.get('interested_supplements') == 'OTHER' and not data.get('interested_supplements_other'):
+            raise serializers.ValidationError({"기타": "기타란에 입력하세요"})
+        return data
 
 class SurveyResultsSerializer(serializers.ModelSerializer):
 
