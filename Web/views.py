1daac9bb52d36d8d4a8b8eeec7d124026cebd505
BeomJung Kim <kbeom0124@naver.com>
2024-05-31 19:18:33 +0900

    fix : 로그인 상태 유지
    로그인 상태 유지를 위한 인터셉터 로직 변경

--- Web/views.py
+++ Web/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, "index.html")
+
 def login(request):
     return render(request, 'account/login.html')
+
+def info_list(request):
+    return render(request, "post/info_list.html")
+
+def info_detail(request, post_pk):
+    context = {
+        'post_pk' : post_pk
+    }
+    return render (request, 'post/info_detail.html', context)
