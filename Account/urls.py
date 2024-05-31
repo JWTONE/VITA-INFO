ce04a9703d30ce484451c39d7f43def687da7fbd
BeomJung Kim <kbeom0124@naver.com>
2024-05-31 10:52:08 +0900

    Add: 로그인 유지 및 auth기능 구현
    axios 인터셉터를 이용해서 토큰 보내는 로직 구현

--- Account/urls.py
+++ Account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import UserListAPIView, LogoutView, PasswordUpdateAPIView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 app_name = "account"
 urlpatterns = [
     path("", UserListAPIView.as_view(), name="user_list"),
+    path("refresh/", TokenRefreshView.as_view(), name="user_list"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("<str:username>/", UserListAPIView.as_view(), name="profile_update"),
