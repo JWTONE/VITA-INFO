dd0df0a98e71109ea025dabeeb20d4297dd6add3
xogns3725 <xogns3725@naver.com>
2024-05-31 19:39:31 +0900

    Add : 프로필페 추가
    mypage.html 추가
    mypage에 개인정보 표시
    views에 get요청 시 개인정보 표시
    mypage url 추가

--- Web/views.py
+++ Web/views.py
@@ -26,3 +26,24 @@ def surveyresult(request):
     context = {'form':SurveyForm}
     return render(request, 'Survey/survey_result.html', context)
 
+def mypage(request, username):
+    return render(request, "account/mypage.html", {"username" : username})
+
+def update(request):
+    pass
+# def update(request):
+#     try:
+#         data = json.loads(request.body)
+#         form = CustomUserChangeForm(data, instance=request.user)
+        
+#         if form.is_valid():
+#             form.save()
+#             return redirect("account:mypage")  # 마이페이지로 리다이렉트
+#         else:
+#             # 유효성 검사에 실패한 경우에 대한 처리
+#             # 예를 들어, 유효성 검사 오류 메시지를 함께 전달하거나 다시 입력 폼을 표시할 수 있습니다.
+#             return render(request, "account/update_failed.html", {"form": form})
+#     except JSONDecodeError:
+#         # 요청의 본문이 유효한 JSON 형식이 아닌 경우에 대한 처리
+#         # 예를 들어, 오류 메시지를 반환하거나 다시 시도하도록 유도할 수 있습니다.
+#         return HttpResponse("Invalid JSON data", status=400)
