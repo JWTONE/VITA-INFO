87088f4b816a8f7558eb4467bc1165d6395abc47
Leejungwoo <soulpc123@gmail.com>
2024-05-31 15:53:49 +0900

    add : survey result 페이지 추가
    설문조사 loading 페이지 추가

--- Web/views.py
+++ Web/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from Survey.forms import SurveyForm
 
 def login(request):
     return render(request, 'account/login.html')
+
+
+def surveymain(request):
+    context = {'form':SurveyForm}
+    return render(request, 'Survey/survey.html', context)
+
+def surveyresult(request):
+    context = {'form':SurveyForm}
+    return render(request, 'Survey/survey_result.html', context)
