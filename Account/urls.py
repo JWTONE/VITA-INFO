2984ea1c1032993fd8f4086c4725952f4d063897
BeomJung Kim <kbeom0124@naver.com>
2024-06-03 16:31:15 +0900

    Update: 글작성 및 댓글/대댓글 기능 추가
    각 게시판에서 글 작성 기능 구현
    상세페이지에서 댓글 및 대댓글 작성 기능 구현
    댓글 및 글 조회시 최신순으로 조회

--- Account/urls.py
+++ Account/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import UserListAPIView, LogoutView, PasswordUpdateAPIView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserListAPIView, LogoutView, PasswordUpdateAPIView, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import  TokenRefreshView
 from . import views
 
 app_name = "account"
 urlpatterns = [
     path("", UserListAPIView.as_view(), name="user_list"),
     path("refresh/", TokenRefreshView.as_view(), name="user_list"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("<str:username>/", UserListAPIView.as_view(), name="profile_update"),
     path("<str:username>/password/",
