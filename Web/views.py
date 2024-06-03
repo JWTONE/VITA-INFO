7d07531bcf1923bc222c0b39a8aece4aec5783b2
xogns3725 <xogns3725@naver.com>
2024-06-03 11:44:24 +0900

    Update : 홈화면 링크추가
    navbar의 링크 누를 경우 각 링크로 이동시키도록 경로 추가
    로그인 시 로그인, 회원가입 안보임
    로그아웃 시 로그아웃 안보임

--- Web/views.py
+++ Web/views.py
@@ -18,6 +18,15 @@ def info_detail(request, post_pk):
     }
     return render (request, 'post/info_detail.html', context)
 
+def review_list(request):
+    return render(request, "post/user_list.html")
+
+def review_detail(request, post_pk):
+    context = {
+        'post_pk' : post_pk
+    }
+    return render (request, 'post/review_detail.html', context)
+
 def surveymain(request):
     context = {'form':SurveyForm}
     return render(request, 'Survey/survey.html', context)
