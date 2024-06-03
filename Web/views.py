1e35c4f01334f26e42f3c50eb47cda6592d214a0
xogns3725 <xogns3725@naver.com>
2024-06-03 14:46:46 +0900

    Update : 회원정보 변경 기능 추가
    mypage에서 회원정보 변경 버튼 클릭 시
    본인 회원정보가 불러와지고 변경가능하도록 함

--- Web/views.py
+++ Web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from Survey.forms import SurveyForm
+from Account.forms import CustomUserChangeForm
+from django.contrib.auth import get_user_model
 
 def index(request):
     return render(request, "index.html")
@@ -47,21 +48,18 @@ def surveyresult(request):
 def mypage(request, username):
     return render(request, "account/mypage.html", {"username" : username})
 
-def update(request):
-    pass
-# def update(request):
-#     try:
-#         data = json.loads(request.body)
-#         form = CustomUserChangeForm(data, instance=request.user)
+def update(request, username):
+    User = get_user_model()
+    user = User.objects.get(username=username)
+    if request.method == 'GET':
+        form = CustomUserChangeForm(instance=user)
+    elif request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
         
-#         if form.is_valid():
-#             form.save()
-#             return redirect("account:mypage")  # 마이페이지로 리다이렉트
-#         else:
-#             # 유효성 검사에 실패한 경우에 대한 처리
-#             # 예를 들어, 유효성 검사 오류 메시지를 함께 전달하거나 다시 입력 폼을 표시할 수 있습니다.
-#             return render(request, "account/update_failed.html", {"form": form})
-#     except JSONDecodeError:
-#         # 요청의 본문이 유효한 JSON 형식이 아닌 경우에 대한 처리
-#         # 예를 들어, 오류 메시지를 반환하거나 다시 시도하도록 유도할 수 있습니다.
-#         return HttpResponse("Invalid JSON data", status=400)
+    context = {
+        'form':form,
+        'username':username
+    }
+    return render(request, 'account/update.html', context)
