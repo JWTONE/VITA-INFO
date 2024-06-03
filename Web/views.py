76099438006269ad426ca8bdcbc6122e18e94fc7
Leejungwoo <soulpc123@gmail.com>
2024-06-03 11:49:21 +0900

    Add : loading page 추가
    main, loading, result로 재구성성

--- Web/views.py
+++ Web/views.py
@@ -22,6 +22,10 @@ def surveymain(request):
     context = {'form':SurveyForm}
     return render(request, 'Survey/survey.html', context)
 
+def surveyloading(request):
+    context = {'form':SurveyForm}
+    return render(request, 'Survey/survey_loading.html', context)
+
 def surveyresult(request):
     context = {'form':SurveyForm}
     return render(request, 'Survey/survey_result.html', context)
