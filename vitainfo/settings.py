ae375648a96632523608f4eca5049657893bcd6f
BeomJung Kim <kbeom0124@naver.com>
2024-06-10 17:13:10 +0900

    fix: 인터셉터 오류
    인터셉터가 리프레시 만료됬을경우 무한반복하는 오류 해결

--- vitainfo/settings.py
+++ vitainfo/settings.py
@@ -129,8 +129,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=31),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
